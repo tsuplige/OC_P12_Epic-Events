349ee38c0d5d9b276d479d511bcc0f800f4c8693
Tsuplige <thomassupligeon@gmail.com>
2024-02-26 16:32:00 +0100

    create login command

--- epic_events/users/management/commands/login.py
+++ epic_events/users/management/commands/login.py
@@ -1,21 +1,31 @@
-# Création d'un nouvel utilisateur
-# from django.contrib.auth.models import User
+import jwt
 from django.core.management.base import BaseCommand
-import os
-from dotenv import load_dotenv
 from django.contrib.auth import authenticate
+from dotenv import load_dotenv
+import os
 
 
 class Command(BaseCommand):
-    help = 'Connecte un utilisateur'
+    help = 'Connecte un utilisateur et enregistre un jeton JWT dans un fichier texte'
 
     def handle(self, *args, **kwargs):
         load_dotenv()
+
         username = os.getenv('USER_NAME')
         password = os.getenv('PASSWORD')
+
         user = authenticate(username=username, password=password)
+
         if user is not None:
-            self.stdout.write(self.style.SUCCESS(f'Connexion réussie {user}'))
+            payload = {'username': user.username,
+                       'department': user.department}
+            jwt_token = jwt.encode(payload, 'secret', algorithm='HS256')
+
+            with open('jwt_token.txt', 'w') as file:
+                file.write(jwt_token)
+
+            self.stdout.write(self.style.SUCCESS(
+                "Connexion réussie. Jeton JWT enregistré dans 'jwt_token.txt'"))
         else:
             self.stdout.write(self.style.ERROR(
-                'Nom d\'utilisateur ou mot de passe incorrect'))
+                "Échec de la connexion. Vérifiez les identifiants dans le fichier .env"))
---
+++ epic_events/users/management/commands/logout.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand
+import os
+
+class Command(BaseCommand):
+    help = 'Déconnecte l\'utilisateur en supprimant le jeton JWT'
+
+    def handle(self, *args, **kwargs):
+        if os.path.exists('jwt_token.txt'):
+            os.remove('jwt_token.txt')
+            self.stdout.write(self.style.SUCCESS("Déconnexion réussie. Le jeton JWT a été supprimé."))
+        else:
+            self.stdout.write(self.style.ERROR("Aucun jeton JWT trouvé. Vous êtes déjà déconnecté."))

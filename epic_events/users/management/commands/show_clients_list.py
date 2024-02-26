034ad6db4ffd46e28dc954c0a1c3891e2fc835a9
Tsuplige <thomassupligeon@gmail.com>
2024-02-26 16:32:40 +0100

    uptdate client view

--- epic_events/users/management/commands/show_clients_list.py
+++ epic_events/users/management/commands/show_clients_list.py
@@ -1,12 +1,17 @@
 from django.core.management.base import BaseCommand
 from users.models import Client
+from users.permissions import is_authenticated
 
 
 class Command(BaseCommand):
     help = 'Affiche la liste des clients'
 
     def handle(self, *args, **kwargs):
-        clients = Client.objects.all()
-        for client in clients:
-            self.stdout.write(self.style.SUCCESS(
-                f"id:{client.id} | name:{client.name} | email:{client.email} | phone:{client.telephone}"))
+
+        if is_authenticated('any'):
+            clients = Client.objects.all()
+            for client in clients:
+                self.stdout.write(self.style.SUCCESS(
+                    f"id:{client.id} | name:{client.name} | email:{client.email} | phone:{client.telephone}"))
+        else:
+            self.stdout.write(self.style.ERROR('vous n\'etes pas autorisé à acceder au données'))
